1f39fcadb4f76cfe629fbdc396bd083d61861eda
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-11-19 16:12:06 -0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import TasksForm from '$lib/TasksForm.svelte';
+</script>
+
+<main>
+	<h1>Tasks App</h1>
+	<TasksForm />
+</main>
+
+<style>
+	main {
+		margin: 1rem auto;
+		max-width: 35rem;
+	}
+</style>
